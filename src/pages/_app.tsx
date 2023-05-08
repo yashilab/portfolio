38838637490da8f206ad18ea2596f1f2b5a474ff
yashi <yasuhito.toyama@yashilab.com>
2023-05-08 16:09:36 +0900

    feat: modern-css-reset を導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import 'modern-css-reset/dist/reset.min.css'
 
 const PortfolioApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
